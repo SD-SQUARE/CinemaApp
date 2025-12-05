8ef2c51b2983edfb39c19c6bf4d8428189fb3f96
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-12-05 04:00:17 +0200

    test: vendor movie list + add + delete

--- client/VendorApp/lib/screens/splash/controller/splash_controller.dart
+++ client/VendorApp/lib/screens/splash/controller/splash_controller.dart
@@ -1,7 +1,6 @@
 class SplashLogic {
   static const List<String> words = [
     "Welcome to Cinmeco Vendor",
-    "Thank you for partnering with us",
     "Preparing your workspace",
     "Checking your inventory",
     "Optimizing your experience",
